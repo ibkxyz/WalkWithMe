946214afaf118f505c0a325354524782cca2727c
ibkxyz <ibkxyz@gmail.com>
2017-06-23 16:16:59 +0100

    Activity updates  in progress

--- app/src/main/java/com/example/abc/walkwithme/Main2Activity.java
+++ app/src/main/java/com/example/abc/walkwithme/Main2Activity.java
@@ -1,13 +1,131 @@
 package com.example.abc.walkwithme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
+
+    private EditText inputName, inputPassword;
+    private TextInputLayout inputLayoutName, inputLayoutPassword;
+    private Button btnSignIn, btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        inputLayoutName = (TextInputLayout) findViewById(R.id.input_layout_name);
+        inputLayoutPassword = (TextInputLayout) findViewById(R.id.input_layout_password);
+        inputName = (EditText) findViewById(R.id.input_name);
+        inputPassword = (EditText) findViewById(R.id.input_password);
+        btnSignIn = (Button) findViewById(R.id.btn_signin);
+        btnSignUp = (Button) findViewById(R.id.btn_signup);
+
+        inputName.addTextChangedListener(new MyTextWatcher(inputName));
+        inputPassword.addTextChangedListener(new MyTextWatcher(inputPassword));
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitForm();
+            }
+        });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signUp();
+            }
+        });
+
+
+    }
+
+    /**
+     * Validating form
+     */
+    private void signUp(){
+        Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
+        startActivity(intent);
+    }
+    private void submitForm() {
+        if (!validateName()) {
+            return;
+        }
+
+        if (!validatePassword()) {
+            return;
+        }
+
+        Toast.makeText(getApplicationContext(), "Thank You!", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(LoginActivity.this, About.class);
+        intent.putExtra("name", inputName.getText().toString());
+        startActivity(intent);
+    }
+
+    private boolean validateName() {
+        if (inputName.getText().toString().trim().isEmpty()) {
+            inputLayoutName.setError(getString(R.string.err_msg_name));
+            requestFocus(inputName);
+            return false;
+        } else {
+            inputLayoutName.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+
+    private boolean validatePassword() {
+        if (inputPassword.getText().toString().trim().isEmpty()) {
+            inputLayoutPassword.setError(getString(R.string.err_msg_password));
+            requestFocus(inputPassword);
+            return false;
+        } else {
+            inputLayoutPassword.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+    private class MyTextWatcher implements TextWatcher {
+
+        private View view;
+
+        private MyTextWatcher(View view) {
+            this.view = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void afterTextChanged(Editable editable) {
+            switch (view.getId()) {
+                case R.id.input_name:
+                    validateName();
+                    break;
+
+                case R.id.input_password:
+                    validatePassword();
+                    break;
+            }
+        }
     }
 }
